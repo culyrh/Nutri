652f5f82ad068c5e251428c71913eac9dda2d914
ubuntu <ict_701@naver.com>
2026-05-04 20:36:56 +0900

    feat: 이름/성별/생년월일 및 신체정보 범위 제한 추가

--- src/main/java/com/example/nutriuniv/domain/auth/dto/RegisterRequest.java
+++ src/main/java/com/example/nutriuniv/domain/auth/dto/RegisterRequest.java
@@ -1,7 +1,6 @@
 package com.example.nutriuniv.domain.auth.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,15 +11,24 @@ import java.time.LocalDate;
 public class RegisterRequest {
     private String provider;
     private String oauthId;
+
+    @NotBlank(message = "올바른 정보를 입력해 주세요.")
+    @Size(min = 2, message = "올바른 정보를 입력해 주세요.")
+    @Pattern(regexp = "^[가-힣a-zA-Z]+$", message = "올바른 정보를 입력해 주세요.")
     private String name;
 
-    @NotBlank(message = "이메일을 입력해 주세요.")
+    @NotBlank(message = "올바른 정보를 입력해 주세요.")
     @Email(
-            message = "올바른 이메일 형식을 입력해 주세요.",
+            message = "올바른 정보를 입력해 주세요.",
             regexp = "^[a-zA-Z0-9._%+\\-]+@[a-zA-Z0-9.\\-]+\\.[a-zA-Z]{2,}$"
     )
     private String email;
 
+    @NotBlank(message = "올바른 정보를 입력해 주세요.")
+    @Pattern(regexp = "^(MALE|FEMALE)$", message = "올바른 정보를 입력해 주세요.")
     private String gender;
+
+    @NotNull(message = "올바른 정보를 입력해 주세요.")
+    @PastOrPresent(message = "올바른 정보를 입력해 주세요.")
     private LocalDate birthDate;
 }
